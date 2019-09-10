ab42b85193693c47c659e2fe6640612b99777431
Daniel Ashcroft <dashcroft4@gmail.com>
2019-09-10 16:48:43 +0100

    App plays video locally stored in raw folder.
    I made some changes to the activity_main.xml to have a VideoView within a frame and to the MainActivity.java to set the VideoView to a uri with the path to the local video along with media controls.

--- app/src/main/java/com/AndroidAppVideoPlayerWithWebServer/videoplayer/MainActivity.java
+++ app/src/main/java/com/AndroidAppVideoPlayerWithWebServer/videoplayer/MainActivity.java
@@ -2,7 +2,10 @@ package com.AndroidAppVideoPlayerWithWebServer.videoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        VideoView  videoView = findViewById(R.id.video_view);
+
+        //My uri to my mp4 video called video.
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+
+        //setting my video view to it.
+        videoView.setVideoURI(uri);
+
+        //To get controls like stop, play etc...
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
